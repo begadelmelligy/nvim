f07354630300916d468e979f48f900cc58902883
Begad Elmelligy <begad-96@hotmail.com>
2025-05-15 20:27:18 -0500

    Added zsh for alt+\ to know when linux s window

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,28 +1,37 @@
 return {
-{
-    "akinsho/toggleterm.nvim",
-    version = "*",
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
     config = function()
-        require("toggleterm").setup({
-            size = function(term)
-                if term.direction == "horizontal" then
-                    return 10
-                elseif term.direction == "vertical" then
-                    return vim.o.columns * 0.4
-                end
-            end,
-            open_mapping = [[<C-\>]], -- Change this shortcut as needed
-            shade_terminals = true,
-            shading_factor = 2,
-            direction = "float", -- This makes it a floating terminal
-            float_opts={
-                    width = 80,
-                    height=30,
-                },
-            close_on_exit = true,
-            persist_mode = true, -- Keeps session open
-            shell = "PowerShell", -- Change to "powershell" if using older Windows PowerShell
-        })
-    end
-}
+      local is_windows = vim.fn.has 'win32' == 1
+      local is_linux = vim.fn.has 'unix' == 1 and not is_windows
+      local shell
+      if is_windows then
+        shell = 'PowerShell'
+      elseif is_linux then
+        shell = 'zsh'
+      end
+
+      require('toggleterm').setup {
+        size = function(term)
+          if term.direction == 'horizontal' then
+            return 10
+          elseif term.direction == 'vertical' then
+            return vim.o.columns * 0.4
+          end
+        end,
+        open_mapping = [[<C-\>]], -- Change this shortcut as needed
+        shade_terminals = true,
+        shading_factor = 2,
+        direction = 'float', -- This makes it a floating terminal
+        float_opts = {
+          width = 80,
+          height = 30,
+        },
+        close_on_exit = true,
+        persist_mode = true, -- Keeps session open
+        shell = shell,
+      }
+    end,
+  },
 }
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -31,13 +31,24 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.api.nvim_set_keymap('i', '(', '()<left>', { noremap = true })
+vim.api.nvim_set_keymap('i', '{', '{}<left>', { noremap = true })
+vim.api.nvim_set_keymap('i', '[', '[]<left>', { noremap = true })
+vim.api.nvim_set_keymap('i', '"', '""<left>', { noremap = true })
+vim.api.nvim_set_keymap('i', "'", "''<left>", { noremap = true })
 
-vim.api.nvim_set_keymap('i', '(', '()<left>', { noremap = true})
-vim.api.nvim_set_keymap('i', '{', '{}<left>', { noremap = true})
-vim.api.nvim_set_keymap('i', '[', '[]<left>', { noremap = true})
-vim.api.nvim_set_keymap('i', '"', '""<left>', { noremap = true})
-vim.api.nvim_set_keymap('i', "'", "''<left>", { noremap = true})
-
+vim.g.clipboard = {
+  name = 'wl-clipboard',
+  copy = {
+    ['+'] = 'wl-copy',
+    ['*'] = 'wl-copy',
+  },
+  paste = {
+    ['+'] = 'wl-paste',
+    ['*'] = 'wl-paste',
+  },
+  cache_enabled = 0,
+}
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
