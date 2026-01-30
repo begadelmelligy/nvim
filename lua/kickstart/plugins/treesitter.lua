8ea3e19222067affe7368ec703df7fe1a461a42b
Begad Elmelligy <begad-96@hotmail.com>
2026-01-29 19:15:13 -0600

    Pine Rose

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -1,17 +1,19 @@
 return {
-  { -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { 'ruby' },
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,    -- 🔥 REQUIRED
+  priority = 1000, -- load early
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter").setup({ -- Changed from nvim-treesitter.configs
+      ensure_installed = {
+        "c",
+        "lua",
+        "python",
+        "vim",
+        "vimdoc",
       },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-  },
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
 }
--- vim: ts=2 sts=2 sw=2 et
